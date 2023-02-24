4ee95799bae67d371fe0252174dc72b2e0c8d8d3
hasan <mdhasanhk.98@gmail.com>
2023-02-24 20:56:05 +0600

    Remainder operator

--- starter/script.js
+++ starter/script.js
@@ -324,3 +324,29 @@ console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
 ///////////////////////////////////////
 */
+
+/*
+///////////////////////////////////////
+//Remainder operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(5 / 2); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(10));
+console.log(isEven(5));
+console.log(isEven(20));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i % 2 === 0) row.style.backgroundColor = '#e52a5a';
+    if (i % 3 === 0) row.style.backgroundColor = 'goldenRod';
+  });
+});
+///////////////////////////////////////
+*/
